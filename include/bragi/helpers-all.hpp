a0d2d8e3747a3114d4a263aa1c4127a4c15928f8
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2021-03-30 15:33:59 +0200

    Generalize write_head_{only,tail} to spans

--- include/bragi/helpers-all.hpp
+++ include/bragi/helpers-all.hpp
@@ -4,8 +4,9 @@
 
 namespace bragi {
 
+// Takes a forwarding reference to be compatible with both containers and spans.
 template <typename Message, typename HBuffer, typename TBuffer>
-inline bool write_head_tail(Message &msg, HBuffer &head, TBuffer &tail) {
+inline bool write_head_tail(Message &msg, HBuffer &&head, TBuffer &&tail) {
 	limited_writer head_rd{head.data(), head.size()};
 	limited_writer tail_rd{tail.data(), tail.size()};
 
@@ -15,8 +16,9 @@ inline bool write_head_tail(Message &msg, HBuffer &head, TBuffer &tail) {
 	return msg.encode_tail(tail_rd);
 }
 
+// Takes a forwarding reference to be compatible with both containers and spans.
 template <typename Message, typename HBuffer>
-inline bool write_head_only(Message &msg, HBuffer &head) {
+inline bool write_head_only(Message &msg, HBuffer &&head) {
 	limited_writer head_rd{head.data(), head.size()};
 
 	return msg.encode_head(head_rd);
